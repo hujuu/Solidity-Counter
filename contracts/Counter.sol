b69ec7d8c0e46d4cafe802508d187307682fad81
hujuu <1961946+hujuu@users.noreply.github.com>
2024-01-07 16:14:28 +0900

    Create Counter.sol

---
+++ contracts/Counter.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Counter {
+    int private count;
+    constructor() {
+        count = 0;
+    }
+    function getCount() public view returns (int) {
+        return count;
+    }
+    function increment() public {
+        count++;
+    }
+    function decrement() public {
+        count--;
+    }
+}
